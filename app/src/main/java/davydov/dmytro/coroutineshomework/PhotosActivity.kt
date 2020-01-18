0c1e3411e527174876f8777b5f23b26f0d3f895c
Dmytro Davydov <DavudD83@gmail.com>
2020-01-18 14:39:07 +0200

    Setup repo

---
+++ app/src/main/java/davydov/dmytro/coroutineshomework/PhotosActivity.kt
@@ -0,0 +1,12 @@
+package davydov.dmytro.coroutineshomework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.photos_activity)
+    }
+}
---
+++ app/src/main/java/davydov/dmytro/coroutineshomework/searchPhotosFlow/SearchPhotosFlowFragment.kt
@@ -0,0 +1,18 @@
+package davydov.dmytro.coroutineshomework.searchPhotosFlow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchPhotosFlowFragment : Fragment()  {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
